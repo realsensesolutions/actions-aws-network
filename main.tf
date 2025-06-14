8d31a701b911c30f640a2ddecc77dcb82564cdf6
Alonso Suarez <alonchb@gmail.com>
2025-06-13 17:52:42 -0700

    add egress only Internet Gateway

--- main.tf
+++ main.tf
@@ -159,59 +159,55 @@ resource "aws_security_group" "public" {
   })
 }
 
-# Public Security Group Rules
-resource "aws_security_group_rule" "public_ingress_tcp_ipv4" {
-  type              = "ingress"
+# Public Security Group Rules (using modern VPC resources)
+resource "aws_vpc_security_group_ingress_rule" "public_tcp_ipv4" {
+  security_group_id = aws_security_group.public.id
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 0
   to_port           = 65535
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.public.id
+  ip_protocol       = "tcp"
+  description       = "Allow all TCP traffic from IPv4"
 }
 
-resource "aws_security_group_rule" "public_ingress_udp_ipv4" {
-  type              = "ingress"
+resource "aws_vpc_security_group_ingress_rule" "public_udp_ipv4" {
+  security_group_id = aws_security_group.public.id
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 0
   to_port           = 65535
-  protocol          = "udp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.public.id
+  ip_protocol       = "udp"
+  description       = "Allow all UDP traffic from IPv4"
 }
 
-resource "aws_security_group_rule" "public_ingress_tcp_ipv6" {
-  type              = "ingress"
+resource "aws_vpc_security_group_ingress_rule" "public_tcp_ipv6" {
+  security_group_id = aws_security_group.public.id
+  cidr_ipv6         = "::/0"
   from_port         = 0
   to_port           = 65535
-  protocol          = "tcp"
-  ipv6_cidr_blocks  = ["::/0"]
-  security_group_id = aws_security_group.public.id
+  ip_protocol       = "tcp"
+  description       = "Allow all TCP traffic from IPv6"
 }
 
-resource "aws_security_group_rule" "public_ingress_udp_ipv6" {
-  type              = "ingress"
+resource "aws_vpc_security_group_ingress_rule" "public_udp_ipv6" {
+  security_group_id = aws_security_group.public.id
+  cidr_ipv6         = "::/0"
   from_port         = 0
   to_port           = 65535
-  protocol          = "udp"
-  ipv6_cidr_blocks  = ["::/0"]
-  security_group_id = aws_security_group.public.id
+  ip_protocol       = "udp"
+  description       = "Allow all UDP traffic from IPv6"
 }
 
-resource "aws_security_group_rule" "public_egress_all_ipv4" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+resource "aws_vpc_security_group_egress_rule" "public_all_ipv4" {
   security_group_id = aws_security_group.public.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+  description       = "Allow all outbound traffic to IPv4"
 }
 
-resource "aws_security_group_rule" "public_egress_all_ipv6" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  ipv6_cidr_blocks  = ["::/0"]
+resource "aws_vpc_security_group_egress_rule" "public_all_ipv6" {
   security_group_id = aws_security_group.public.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1"
+  description       = "Allow all outbound traffic to IPv6"
 }
 
 # Private Security Group - allows traffic only within VPC
@@ -226,57 +222,53 @@ resource "aws_security_group" "private" {
   })
 }
 
-# Private Security Group Rules
-resource "aws_security_group_rule" "private_ingress_tcp_ipv4" {
-  type              = "ingress"
+# Private Security Group Rules (using modern VPC resources)
+resource "aws_vpc_security_group_ingress_rule" "private_tcp_ipv4" {
+  security_group_id = aws_security_group.private.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
   from_port         = 0
   to_port           = 65535
-  protocol          = "tcp"
-  cidr_blocks       = [aws_vpc.main.cidr_block]
-  security_group_id = aws_security_group.private.id
+  ip_protocol       = "tcp"
+  description       = "Allow TCP traffic from VPC IPv4"
 }
 
-resource "aws_security_group_rule" "private_ingress_udp_ipv4" {
-  type              = "ingress"
+resource "aws_vpc_security_group_ingress_rule" "private_udp_ipv4" {
+  security_group_id = aws_security_group.private.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
   from_port         = 0
   to_port           = 65535
-  protocol          = "udp"
-  cidr_blocks       = [aws_vpc.main.cidr_block]
-  security_group_id = aws_security_group.private.id
+  ip_protocol       = "udp"
+  description       = "Allow UDP traffic from VPC IPv4"
 }
 
-resource "aws_security_group_rule" "private_ingress_tcp_ipv6" {
-  type              = "ingress"
+resource "aws_vpc_security_group_ingress_rule" "private_tcp_ipv6" {
+  security_group_id = aws_security_group.private.id
+  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
   from_port         = 0
   to_port           = 65535
-  protocol          = "tcp"
-  ipv6_cidr_blocks  = [aws_vpc.main.ipv6_cidr_block]
-  security_group_id = aws_security_group.private.id
+  ip_protocol       = "tcp"
+  description       = "Allow TCP traffic from VPC IPv6"
 }
 
-resource "aws_security_group_rule" "private_ingress_udp_ipv6" {
-  type              = "ingress"
+resource "aws_vpc_security_group_ingress_rule" "private_udp_ipv6" {
+  security_group_id = aws_security_group.private.id
+  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
   from_port         = 0
   to_port           = 65535
-  protocol          = "udp"
-  ipv6_cidr_blocks  = [aws_vpc.main.ipv6_cidr_block]
-  security_group_id = aws_security_group.private.id
+  ip_protocol       = "udp"
+  description       = "Allow UDP traffic from VPC IPv6"
 }
 
-resource "aws_security_group_rule" "private_egress_all_ipv4" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+resource "aws_vpc_security_group_egress_rule" "private_all_ipv4" {
   security_group_id = aws_security_group.private.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+  description       = "Allow all outbound traffic to IPv4"
 }
 
-resource "aws_security_group_rule" "private_egress_all_ipv6" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  ipv6_cidr_blocks  = ["::/0"]
+resource "aws_vpc_security_group_egress_rule" "private_all_ipv6" {
   security_group_id = aws_security_group.private.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1"
+  description       = "Allow all outbound traffic to IPv6"
 }
